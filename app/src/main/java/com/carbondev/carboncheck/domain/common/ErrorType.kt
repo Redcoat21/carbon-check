97c3c774007f0f0c9ff009050afd825be4d370e4
Dahren Ericsson <qvbo7teu@registrat21.anonaddy.me>
2025-05-19 11:53:06 +0700

    refactor: Moved ErrorType enum to its own file.

---
+++ app/src/main/java/com/carbondev/carboncheck/domain/common/ErrorType.kt
@@ -0,0 +1,9 @@
+package com.carbondev.carboncheck.domain.common
+
+enum class ErrorType {
+    NETWORK_ERROR,
+    DATABASE_ERROR,
+    UNKNOWN_ERROR,
+    NOT_FOUND_ERROR,
+    VALIDATION_ERROR,
+}
--- app/src/main/java/com/carbondev/carboncheck/domain/common/Result.kt
+++ app/src/main/java/com/carbondev/carboncheck/domain/common/Result.kt
@@ -9,9 +9,3 @@ sealed class Result<out T> {
     ) : Result<Nothing>()
 }
 
-enum class ErrorType {
-    NETWORK_ERROR,
-    DATABASE_ERROR,
-    UNKNOWN_ERROR,
-    NOT_FOUND_ERROR
-}
