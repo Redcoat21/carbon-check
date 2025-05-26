3dbe5e4cfa2bd117f7633d1c24d39e86e0cb6961
Dahren Ericsson <qvbo7teu@registrat21.anonaddy.me>
2025-05-26 23:12:21 +0700

    refactor: Changed the Network model from kotlinx.serialization to moshi.

--- app/src/main/java/com/carbondev/carboncheck/data/remote/model/NetworkUser.kt
+++ app/src/main/java/com/carbondev/carboncheck/data/remote/model/NetworkUser.kt
@@ -1,20 +1,19 @@
 package com.carbondev.carboncheck.data.remote.model
 
-import kotlinx.serialization.Serializable
 import com.carbondev.carboncheck.domain.model.User
 import com.squareup.moshi.Json
-import kotlinx.serialization.Contextual
+import com.squareup.moshi.JsonClass
 import java.util.Date
 
-@Serializable
+@JsonClass(generateAdapter = true)
 data class NetworkUser(
     val id: String,
     @Json(name = "first_name") val firstName: String,
     @Json(name = "last_name") val lastName: String,
     val email: String,
-    @Json(name = "created_at") @Contextual val createdAt: Date,
-    @Json(name = "updated_at") @Contextual val updatedAt: Date,
-    @Json(name = "deleted_at") @Contextual val deletedAt: Date?,
+    @Json(name = "created_at") val createdAt: Date,
+    @Json(name = "updated_at") val updatedAt: Date,
+    @Json(name = "deleted_at") val deletedAt: Date?,
     @Json(name = "avatar_url") val avatarUrl: String
 ) : RemoteMappable<User> {
     companion object {
--- app/src/main/java/com/carbondev/carboncheck/data/remote/model/NetworkVoucher.kt
+++ app/src/main/java/com/carbondev/carboncheck/data/remote/model/NetworkVoucher.kt
@@ -1,20 +1,19 @@
 package com.carbondev.carboncheck.data.remote.model
 
-import kotlinx.serialization.Serializable
 import com.carbondev.carboncheck.domain.model.Voucher
 import com.squareup.moshi.Json
-import kotlinx.serialization.Contextual
+import com.squareup.moshi.JsonClass
 import java.util.Date
 
-@Serializable
+@JsonClass(generateAdapter = true)
 data class NetworkVoucher(
     val id: String,
     val vendor: NetworkVendor,
     val name: String,
     val amount: Int,
-    @Json(name = "created_at") @Contextual val createdAt: Date,
-    @Json(name = "updated_at") @Contextual val updatedAt: Date,
-    @Json(name = "deleted_at") @Contextual val deletedAt: Date?,
+    @Json(name = "created_at") val createdAt: Date,
+    @Json(name = "updated_at") val updatedAt: Date,
+    @Json(name = "deleted_at") val deletedAt: Date?,
 ) : RemoteMappable<Voucher> {
     override fun toDomainModel(): Voucher {
         return Voucher(
