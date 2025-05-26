2624d4382d9c3c79b42ab4dd96137f2cc1d5190d
Dahren Ericsson <qvbo7teu@registrat21.anonaddy.me>
2025-05-26 23:04:14 +0700

    test: Added ErrorHandlerTest

---
+++ app/src/test/java/com/carbondev/carboncheck/domain/error/ErrorHandlerTest.kt
@@ -0,0 +1,24 @@
+package com.carbondev.carboncheck.domain.error
+
+import com.carbondev.carboncheck.data.error.ErrorHandlerImplementation
+import com.carbondev.carboncheck.domain.common.ErrorType
+import org.junit.Test
+import junit.framework.TestCase.assertEquals
+
+class ErrorHandlerTest {
+    private val errorHandler = ErrorHandlerImplementation()
+
+    @Test
+    fun `mapToDomainError should return NETWORK_ERROR for IOException`() {
+        val exception = java.io.IOException("Network error")
+        val result = errorHandler.mapToDomainError(exception)
+        assertEquals(result, ErrorType.NETWORK_ERROR)
+    }
+
+    @Test
+    fun `mapToDomainError should return UNKNOWN_ERROR for other exceptions`() {
+        val exception = Exception("Unknown error")
+        val result = errorHandler.mapToDomainError(exception)
+        assertEquals(result, ErrorType.UNKNOWN_ERROR)
+    }
+}
