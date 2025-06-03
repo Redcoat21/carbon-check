54dcc0287f0c0fa213e3aeefa4aa3e19691b05a1
Dahren Ericsson <qvbo7teu@registrat21.anonaddy.me>
2025-06-03 21:07:27 +0700

    feat: Added VoucherRemoteDataSource

--- app/src/main/java/com/carbondev/carboncheck/data/remote/model/NetworkVoucher.kt
+++ app/src/main/java/com/carbondev/carboncheck/data/remote/model/NetworkVoucher.kt
@@ -14,6 +14,24 @@ data class NetworkVoucher(
     @Json(name = "created_at") val createdAt: Date,
     @Json(name = "updated_at") val updatedAt: Date,
 ) : RemoteMappable<Voucher> {
+
+    companion object {
+        const val TABLE_NAME = "vouchers"
+    }
+
+    object Columns {
+        const val ID = "id"
+        const val VENDOR_ID = "vendor"
+        const val CREATED_AT = "created_at"
+        const val UPDATED_AT = "updated_at"
+
+        val ALL = listOf(
+            ID,
+            VENDOR_ID,
+            CREATED_AT,
+            UPDATED_AT,
+        )
+    }
     override fun toDomainModel(): Voucher {
         return Voucher(
             id = id,
