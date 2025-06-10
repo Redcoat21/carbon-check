80b6b1139e9dfe5cbb768fbe19ee01a41b2088d0
Dahren Ericsson <qvbo7teu@registrat21.anoaddy.me>
2025-06-10 19:56:39 +0700

    feat: Added RepositoryModule for dependency injection for repository.

---
+++ app/src/main/java/com/carbondev/carboncheck/di/RepositoryModule.kt
@@ -0,0 +1,35 @@
+package com.carbondev.carboncheck.di
+
+import com.carbondev.carboncheck.data.remote.repository.UserRepositoryRemoteImplementation
+import com.carbondev.carboncheck.data.remote.repository.VendorRepositoryRemoteImplementation
+import com.carbondev.carboncheck.data.remote.repository.VoucherRepositoryImplementation
+import com.carbondev.carboncheck.domain.repository.UserRepository
+import com.carbondev.carboncheck.domain.repository.VendorRepository
+import com.carbondev.carboncheck.domain.repository.VoucherRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideUserRepository(impl: UserRepositoryRemoteImplementation): UserRepository {
+        return impl
+    }
+
+    @Provides
+    @Singleton
+    fun provideVendorRepository(impl: VendorRepositoryRemoteImplementation): VendorRepository {
+        return impl
+    }
+
+    @Provides
+    @Singleton
+    fun provideVoucherRepository(impl: VoucherRepositoryImplementation): VoucherRepository {
+        return impl
+    }
+}
