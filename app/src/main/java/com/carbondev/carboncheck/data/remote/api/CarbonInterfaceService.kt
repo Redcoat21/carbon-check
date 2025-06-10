dc72e8381fc6113b72fc21c336352781fc0e38fa
Dahren Ericsson <qvbo7teu@registrat21.anonaddy.me>
2025-06-10 20:50:45 +0700

    feat: Added service for the shipping estimation

--- app/src/main/java/com/carbondev/carboncheck/data/remote/api/CarbonInterfaceService.kt
+++ app/src/main/java/com/carbondev/carboncheck/data/remote/api/CarbonInterfaceService.kt
@@ -3,8 +3,10 @@ package com.carbondev.carboncheck.data.remote.api
 import com.carbondev.carboncheck.data.remote.model.NetworkResponse
 import com.carbondev.carboncheck.data.remote.model.attribute.NetworkElectricityAttribute
 import com.carbondev.carboncheck.data.remote.model.attribute.NetworkFlightAttribute
+import com.carbondev.carboncheck.data.remote.model.attribute.NetworkShippingAttribute
 import com.carbondev.carboncheck.data.remote.model.request.NetworkElectricityRequest
 import com.carbondev.carboncheck.data.remote.model.request.NetworkFlightRequest
+import com.carbondev.carboncheck.data.remote.model.request.NetworkShippingRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -14,4 +16,7 @@ interface CarbonInterfaceService {
 
     @POST
     suspend fun getFlightEstimation(@Body request: NetworkFlightRequest): NetworkResponse<NetworkFlightAttribute>
+
+    @POST
+    suspend fun getShippingEstimation(@Body request: NetworkShippingRequest): NetworkResponse<NetworkShippingAttribute>
 }
---
+++ app/src/main/java/com/carbondev/carboncheck/data/remote/model/attribute/NetworkShippingAttribute.kt
@@ -0,0 +1,27 @@
+package com.carbondev.carboncheck.data.remote.model.attribute
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.datetime.Instant
+
+/**
+ * DTO for the attribute in shipping response from the Carbon Interface API.
+ * @property distanceValue the value of distance in the specified unit.
+ * @property distanceUnit the unit of distance (e.g., "km", "mi").
+ * @property weightValue the value of weight in the specified unit.
+ * @property weightUnit the unit of weight (e.g., "g", "lb", "kg", "mt").
+ * @property transportMethod the method of transport (e.g., "truck", "train", "plane", "ship").
+ */
+@JsonClass(generateAdapter = true)
+data class NetworkShippingAttribute(
+    @Json(name = "distance_value") val distanceValue: Double,
+    @Json(name = "distance_unit") val distanceUnit: String,
+    @Json(name = "weight_value") val weightValue: Double,
+    @Json(name = "weight_unit") val weightUnit: String,
+    @Json(name = "transport_method") val transportMethod: String,
+    @Json(name = "estimated_at") override val estimatedAt: Instant,
+    @Json(name = "carbon_g") override val carbonG: Double,
+    @Json(name = "carbon_lb") override val carbonLb: Double,
+    @Json(name = "carbon_kg") override val carbonKg: Double,
+    @Json(name = "carbon_mt") override val carbonMt: Double,
+) : NetworkAttributeResponse()
