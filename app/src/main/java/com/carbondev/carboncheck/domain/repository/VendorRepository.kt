2ac8c0718ab61cd228e5634bd1713d31e72e6df0
Dahren Ericsson <qvbo7teu@registrat21.anonaddy.me>
2025-06-19 12:29:37 +0700

    chore: Added javadoc comment

--- app/src/main/java/com/carbondev/carboncheck/domain/repository/VendorRepository.kt
+++ app/src/main/java/com/carbondev/carboncheck/domain/repository/VendorRepository.kt
@@ -7,5 +7,9 @@ import com.carbondev.carboncheck.domain.model.Vendor
  * Repository interface to manage vendor data.
  */
 interface VendorRepository {
+    /**
+     * Fetches a list of vendors.
+     * @return [Result] containing a list of vendors or an error.
+     */
     suspend fun getVendor(id: String): Result<Vendor?>
 }
--- app/src/main/java/com/carbondev/carboncheck/domain/repository/VoucherRepository.kt
+++ app/src/main/java/com/carbondev/carboncheck/domain/repository/VoucherRepository.kt
@@ -4,7 +4,21 @@ import com.carbondev.carboncheck.domain.common.Result
 import com.carbondev.carboncheck.domain.model.Voucher
 import com.carbondev.carboncheck.domain.model.VoucherIdentifier
 
+/**
+ * Repository interface for managing vouchers.
+ */
 interface VoucherRepository {
+    /**
+     * Get a list of vouchers.
+     * @param identifier Optional identifier to filter vouchers. See [VoucherIdentifier]
+     * @return [Result] containing a list of vouchers or an error.
+     */
     suspend fun getVouchers(identifier: VoucherIdentifier? = null): Result<List<Voucher>>
+
+    /**
+     * Get a specific voucher by its ID.
+     * @param id The unique identifier of the voucher.
+     * @return [Result] containing the voucher or an error.
+     */
     suspend fun getVoucher(id: String): Result<Voucher>
 }
